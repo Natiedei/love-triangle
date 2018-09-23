02037ac83984d0acb773de9a5f2c6f7139de8643
kate.d <decpumes@gmail.com>
2018-09-23 20:33:45 +0300

    lov^triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var kol, per1, per2, per3;
+  kol = 0;
+  for(var i=0;i<preferences.length;i++){
+	  per1 = preferences[i]-1;
+	  per2 = preferences[per1]-1;
+	  per3 = preferences[per2]-1;
+	if ((per3 == i) && (per1 != per2) && (per2 != per3) && (per1 != per3)){
+		kol++;
+	}
+  }
+  if (kol > 0) kol = kol / 3; 
+  return kol;
 };
